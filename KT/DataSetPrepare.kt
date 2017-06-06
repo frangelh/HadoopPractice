dfb2cdbcad8af436769433ba08609acc2114cd84
Frangel Hernandez <fhernandez@avathartech.com>
2017-06-06 18:00:32 -0400

    Cambiando tiendas a ciudades y probando

--- KT/DataSetPrepare.kt
+++ KT/DataSetPrepare.kt
@@ -4,11 +4,11 @@ import java.security.SecureRandom
 data class Tienda(var tienda: String, var mes: String, var monto: Int)
 fun main(args: Array<String>) {
     val listado: ArrayList<Tienda> = ArrayList()
-    val tiendas: List<String> = listOf("Macdonalds", "BurgerKing", "Quiznos", "Dominos", "PizzaHut", "HeladosBon", "Pizzarelli", "KFC")
+    val tiendas: List<String> = listOf("Santiago", "LaVega", "Salcedo", "Moca", "Trinidad", "Duarte", "SantoDomingo", "LaRomana")
     val meses: List<String> = listOf("ene", "feb", "mar", "abr", "may", "jun", "jul", "ago", "sep", "oct", "nov", "dic")
     for (i in 1 until 10000) 
         listado.add(Tienda(tiendas[SecureRandom().nextInt(7)], meses[SecureRandom().nextInt(12)], SecureRandom().nextInt(5000)))
-    File("./input").printWriter().use { out ->
+    File("../input").printWriter().use { out ->
         listado.forEach { out.println("${it.tienda} \t ${it.mes} \t ${it.monto}") }
     }
 }
